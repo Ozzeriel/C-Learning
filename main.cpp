770cef4b06f388014bf7d6396000f447333af030
ozzeriel <rubentz97@gmail.com>
2018-07-22 01:37:20 +0200

    .

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main (int argc, char const *argv[])
     // This line is added just to test. 
     
     std::cout << "Hello world" << std::endl;
+    std::cout << "Bye" << std::endl;
     return 0;
 }
